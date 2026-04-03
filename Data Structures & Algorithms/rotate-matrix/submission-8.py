90d4cf6e3b9160ac1c6ccd61806418bab4b0d7c4
Lobarev Pavel <34250546+OZIOisgood@users.noreply.github.com>
2026-04-03 23:58:12 +0200

    Add: rotate-matrix - submission-8

---
+++ Data Structures & Algorithms/rotate-matrix/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        l = 0; r = len(matrix) - 1
+
+        while l < r:
+            for i in range(r - l):
+                tmp = matrix[l][l + i]
+                matrix[l][l + i] = matrix[r - i][l]
+                matrix[r - i][l] = matrix[r][r - i]
+                matrix[r][r - i] = matrix[l + i][r]
+                matrix[l + i][r] = tmp
+            l += 1; r -= 1
